00e88bb0adf8680d4b51c3cd131a644c72f16f13
João Victor Barbosa <shddml@hotmail.com>
2025-08-01 13:43:47 -0300

    post por patch

--- api/detectar-produto.js
+++ api/detectar-produto.js
@@ -105,7 +105,7 @@ async function criarOpcaoNoCampo(customFieldId, contextId, novoValor, auth, base
 // 4. Atualizar campo
 async function atualizarCampoProdutoNaIssue(issueKey, produto, auth, baseUrl) {
   const customFieldId = "customfield_10878";
-  await axios.post(
+  await axios.patch(
     `${baseUrl}/rest/api/3/issue/${issueKey}`,
     { fields: { [customFieldId]: { value: produto } } },
     { auth }
